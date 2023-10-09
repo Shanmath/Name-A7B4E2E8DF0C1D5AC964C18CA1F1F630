a0d94253abb1ef0d43d20f1769078977bc5a76fa
Shanmath <144346107+Shanmath@users.noreply.github.com>
2023-10-09 19:31:20 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,12 @@
+'''Implement a function called sort students that takes a list of student objects as input and sorts the list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function with different input lists of students.'''
+# python
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
+    return sorted_students
+
